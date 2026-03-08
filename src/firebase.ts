f7c90aad025f11e13f12be1ac9c59cd5e617d562
sumanpriya9973-ux <sumanpriya9973@gmail.com>
2026-03-08 07:18:27 +0530

    refactor(firebase): Use getFirestore for Firestore initialization
    Replaces `initializeFirestore` with the more standard `getFirestore` function for initializing the Firestore instance. The `experimentalForceLongPolling` option is no longer necessary or available in newer versions of the Firebase SDK.

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import { initializeFirestore } from "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCulss077QpKWLha3aO7Hj6e4LZYUCyc_M",
@@ -14,7 +14,5 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const db = initializeFirestore(app, {
-  experimentalForceLongPolling: true,
-});
+export const db = getFirestore(app);
 export const googleProvider = new GoogleAuthProvider();
